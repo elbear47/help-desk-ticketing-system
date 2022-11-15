12b968ba603b69cac2763e5e24747f16652f7ba6
Elber Funez <elberfunez@gmail.com>
2022-11-14 22:14:56 -0500

    modified create ticket component to use service

--- src/app/components/create-ticket/create-ticket.component.ts
+++ src/app/components/create-ticket/create-ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TicketService } from '../../services/ticket.service';
 
 @Component({
   selector: 'app-create-ticket',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-ticket.component.css']
 })
 export class CreateTicketComponent implements OnInit {
+  _title: string = '';
+  _priority: number = 0;
+  _details: string = '';
+  _submittedBy: string = '';
+  _userId: number = 0;
+  _ticketId: number = 2;
+  _dateSubmitted: string = '';
+  _resolvedBy: string = '';
+  _resolutionNote: string = '';
+  _active: boolean = true;
 
-  constructor() { }
+
+
+  constructor(private ticketService: TicketService) { }
 
   ngOnInit(): void {
+  }
+
+  addNewTicket(
+    _title: string,
+    _priority: number,
+    _details: string,
+    _submittedBy: string,
+    _userId: number
+  ){
+    this.ticketService.createAndAddNewTicket(_title, _priority, _details, _submittedBy, _userId)
   }
 
 }
--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -5,7 +5,7 @@ import { Ticket } from '../interfaces/Ticket';
   providedIn: 'root',
 })
 export class TicketService {
-// test it with dummy data
+  // test it with dummy data
   ticketOne: Ticket = {
     ticketId: 1,
     title: 'Computer not working',
@@ -15,8 +15,8 @@ export class TicketService {
     submittedBy: 'Jacob Magyar',
     userId: 1,
     resolvedBy: 'Elber Funez',
-    resolutionNote:'',
-    active: true
+    resolutionNote: '',
+    active: true,
   };
 
   currentId: number = 0;
@@ -30,14 +30,39 @@ export class TicketService {
   }
 
   getTicketById(id: number): Ticket {
-    return this.ticketList.filter(t => t.ticketId == id)[0];
+    return this.ticketList.filter((t) => t.ticketId == id)[0];
   }
-  setCurrentId(id: number): number{
+  setCurrentId(id: number): number {
     this.currentId = id;
     return this.currentId;
   }
 
-  getCurrentId(){
+  getCurrentId(): number {
     return this.currentId;
   }
+
+  createAndAddNewTicket(
+    _title: string,
+    _priority: number,
+    _details: string,
+    _submittedBy: string,
+    _userId: number
+  ): void {
+    let newTicket = {
+      ticketId: 2,
+      title: _title,
+      dateSubmitted: '2022-01-05',
+      priority: _priority,
+      details: _details,
+      submittedBy: _submittedBy,
+      userId: _userId,
+      resolvedBy: '',
+      resolutionNote: '',
+      active: true
+    };
+    this.ticketList.push(newTicket)
+
+  }
+
+
 }
